eff9e217dcf55b662a72a2be39ebadc50480b54c
myke47 <47667530+myke47@users.noreply.github.com>
2019-11-21 20:14:17 -0700

    Action creator established

--- react-redux/src/index.js
+++ react-redux/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import  configRedux  from '../configRedux';
+import  configRedux  from './configRedux';
 import { Provider } from 'react-redux';
 
 const store = configRedux();
